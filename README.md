322542a61d4996b944487cc100caec4a849bf807
Thibaut Gauvin <gauvin.thibaut83@gmail.com>
2017-01-12 01:01:43 +0100

    init project

---
+++ README.md
@@ -0,0 +1,40 @@
+# sass-example
+
+you need to install on your local machine:
+
+
+- node js ( & also npm)
+
+That's all !
+
+* git clone the project
+
+* Cd to the project
+
+    $ npm install
+    
+* Install bootsrap & jquery with bower
+
+    $ npm run bower install
+    
+* Compile Sass file & dump js file to the dist folder
+
+    $ npm run gulp build
+    
+
+    
+Take a look at index.html
+
+
+Enjoy <3
+
+
+###################
+
+
+The structure: 
+
+* Recipes for compiling sass are located in the gulpfile.js at the project root, take a look to it !
+* Sass sources are located in 'assets/sass'
+* Javascript sources are located in 'assets/js'
+* The compiled files are located in dist folder DONT EDIT THEM ! even you recompile, your changes will be override !!!!
---
+++ gulpfile.js
@@ -0,0 +1,75 @@
+'use strict';
+
+/**
+ * Libs & Plugin
+ */
+var gulp        = require('gulp');
+var sass        = require('gulp-sass');
+var changed     = require('gulp-changed');
+var imagemin    = require('gulp-imagemin');
+var svgstore    = require('gulp-svgstore');
+
+/**
+ * Variable used in recipes
+ */
+var config = {
+    bowerDir: './bower_components',
+    sassSrc: 'assets/sass/**/*.scss',
+    sassDest: './dist/css',
+    sassOutputStyle: 'compressed',
+    jsSrc: 'assets/js/**/*.js',
+    jsDest: './dist/js',
+    svgSrc: 'assets/icons/**/*.svg',
+    svgDest: './dist/icons'
+};
+
+gulp.task( 'default',
+    [ 'build']
+);
+
+gulp.task( 'build',
+    [ 'svgSprite', 'css', 'js' ]
+);
+
+gulp.task( 'dist',
+    [ 'build' ]
+);
+
+/**
+ * Compile Sass to css
+ */
+gulp.task( 'css', function() {
+    gulp.src([
+        config.sassSrc
+    ])
+    .pipe(
+        sass({
+            outputStyle: config.sassOutputStyle
+        }).on('error', sass.logError)
+    )
+    .pipe(gulp.dest(config.sassDest))
+});
+
+/**
+ * Dump Javascript from 'assets/js' TO 'dist/js' folder
+ */
+gulp.task( 'js', function() {
+    gulp.src(
+        [
+            config.bowerDir + '/bootstrap-sass/assets/javascripts/bootstrap.min.js',
+            config.bowerDir + '/jquery/dist/jquery.min.js',
+            config.jsSrc
+        ])
+        .pipe(changed(config.jsDest))
+        .pipe(gulp.dest(config.jsDest))
+});
+
+/**
+ * Watch for change
+ *   - Recompile Sass
+ *   - Dump Js
+ */
+gulp.task('watch', function () {
+    gulp.watch(config.sassSrc, ['css']);
+    gulp.watch(config.jsSrc, ['js']);
+});
